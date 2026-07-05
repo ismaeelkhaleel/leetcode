ef14059d5db9377b289c6df3c78c09e6c8a3e41f
Mohd Ismaeel <pmohd367@gmail.com>
2026-07-05 19:22:19 +0530

    Add Solution for Fibonacci Number (Method 2)

---
+++ 0509 - Fibonacci Number/Method 2/solution.java
@@ -0,0 +1,7 @@
+    public int solve(int n,  int[] dp) {
+        return solve(n, dp);
+    }
+        Arrays.fill(dp,-1);
+    public int fib(int n) {
+        int[] dp = new int[n+1];
+class Solution {
