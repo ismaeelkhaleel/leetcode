2496b8a85a4948d1ada03f09f62f0628142625e5
Mohd Ismaeel <pmohd367@gmail.com>
2026-07-05 19:16:18 +0530

    Add solution for Fibonacci Number

---
+++ Fibonacci Number/solution.java
@@ -0,0 +1,7 @@
+        dp[1] = 1;
+        for(int i = 2; i<=n; i++) {
+        dp[0] = 0;
+        int[] dp = new int[n+1];
+        if(n==0) return 0;
+class Solution {
+    public int fib(int n) {
