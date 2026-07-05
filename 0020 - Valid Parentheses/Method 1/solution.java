063aca00b52f0d037ab9fb12d51f384046bd3f71
Mohd Ismaeel <pmohd367@gmail.com>
2026-07-05 22:47:46 +0530

    Add Solution for Valid Parentheses (Method 1)

---
+++ 0020 - Valid Parentheses/Method 1/solution.java
@@ -0,0 +1,16 @@
+                if(stack.isEmpty()) {
+                    return false;
+                 } else if((stack.peek()=='
+                 (' && c==')') || (stack.peek()
+                 =='[' && c==']') || (stack.peek
+                 ()=='{' && c=='}')) {
+                    stack.pop();
+            if(c==')'  || c=='}' ||  c==']'  ) {
+                stack.push(c);
+            }
+            if(c=='(' || c=='{' || c=='[') {
+            char c = s.charAt(i);
+        for(int i = 0; i<s.length(); i++) {
+        Stack<Character> stack = new Stack<>();
+    public boolean isValid(String s) {
+class Solution {
