2871a0442a3e901812b87a949fea64326b5c9d5b
Mohd Ismaeel <pmohd367@gmail.com>
2026-08-20 15:09:59 +0530

    Add Solution for Cinema Seat Allocation (Method 1)

---
+++ 1386 - Cinema Seat Allocation/Method 1/solution.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
+
+        Map<Integer, Set<Integer>> map = new HashMap<>();
+
+        for (int[] seat : reservedSeats) {
+            map.computeIfAbsent(seat[0], k -> new HashSet<>())
+               .add(seat[1]);
+        }
+
+        int count = (n - map.size()) * 2;
+
+        for (Set<Integer> seats : map.values()) {
+
+            boolean left = true;
+            boolean middle = true;
+            boolean right = true;
+            for (int seat : seats) {
+
+                if (seat >= 2 && seat <= 5) {
+                    left = false;
+                }
+
+                if (seat >= 4 && seat <= 7) {
+                    middle = false;
+                }
+
+                if (seat >= 6 && seat <= 9) {
+                    right = false;
+                }
+            }
+
+            if (left && right) {
+                count += 2;
+            } else if (left || middle || right) {
+                count += 1;
+            }
+        }
+
+        return count;
+    }
+}
